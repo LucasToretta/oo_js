8c7692a180e94399045e4375721390fd06be14a7
Lucas Toretta <lucastoretta@hotmail.com>
2025-09-05 11:42:43 -0300

    save completo

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function Encomenda(massa, molhadoCom, recheio, cobertura) {
+function Encomenda(nomeCliente, massa, molhadoCom, recheio, cobertura) {
+    this.nomeCliente = nomeCliente;
     this.massa = massa;
     this.molhadoCom = molhadoCom;
     this.recheio = recheio;
@@ -6,27 +7,47 @@ function Encomenda(massa, molhadoCom, recheio, cobertura) {
 }
 
 Encomenda.prototype.getDescricao = function() {
-    return `O bolo será com a massa ${this.massa}, molhado com ${this.molhadoCom}, terá o recheio de ${this.recheio} e será com a cobertura de ${this.cobertura}.`;
+    let tipoTexto = this.tipo ? `${this.tipo}: ` : "";
+    return `${tipoTexto}O bolo do(a) cliente ${this.nomeCliente} será com a massa ${this.massa}, molhado com ${this.molhadoCom}, terá o recheio de ${this.recheio} e será com a cobertura de ${this.cobertura}.`;
 };
 
-function BoloDeCasamento(massa, molhadoCom, recheio, cobertura) {
-    Encomenda.call(this, massa, molhadoCom, recheio, cobertura);
+function BoloDeCasamento(nomeCliente, massa, molhadoCom, recheio, cobertura) {
+    Encomenda.call(this, nomeCliente, massa, molhadoCom, recheio, cobertura);
     this.tipo = "Bolo de Casamento";
 }
 BoloDeCasamento.prototype = Object.create(Encomenda.prototype);
 BoloDeCasamento.prototype.constructor = BoloDeCasamento;
 
-function BoloDeAniversario(massa, molhadoCom, recheio, cobertura) {
-    Encomenda.call(this, massa, molhadoCom, recheio, cobertura);
+function BoloDeAniversario(nomeCliente, massa, molhadoCom, recheio, cobertura) {
+    Encomenda.call(this, nomeCliente, massa, molhadoCom, recheio, cobertura);
     this.tipo = "Bolo de Aniversário";
 }
 BoloDeAniversario.prototype = Object.create(Encomenda.prototype);
 BoloDeAniversario.prototype.constructor = BoloDeAniversario;
 
-const BoloDeCasamentoDaMaria = new BoloDeCasamento("Chocolate", "leite", "trufa", "ganache");
-const BoloDeAniversarioDoManoel = new BoloDeAniversario("Branca", "licor", "creme", "chantilly");
-const BoloDeCasamentoDoJoaquim = new BoloDeCasamento("Chocolate", "guaraná", "brigadeiro", "buttercream");
+const form = document.getElementById("formPedido");
+const pedidosDiv = document.getElementById("pedidos");
 
-console.log(BoloDeAniversarioDoManoel.getDescricao());
-console.log(BoloDeCasamentoDaMaria.getDescricao());
-console.log(BoloDeCasamentoDoJoaquim.getDescricao());
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const nomeCliente = document.getElementById("nomeCliente").value;
+    const massa = document.getElementById("massa").value;
+    const molhadoCom = document.getElementById("molhadoCom").value;
+    const recheio = document.getElementById("recheio").value;
+    const cobertura = document.getElementById("cobertura").value;
+    const tipo = document.getElementById("tipoBolo").value;
+
+    let bolo;
+    if(tipo === "casamento") {
+        bolo = new BoloDeCasamento(nomeCliente, massa, molhadoCom, recheio, cobertura);
+    } else {
+        bolo = new BoloDeAniversario(nomeCliente, massa, molhadoCom, recheio, cobertura);
+    }
+
+    const p = document.createElement("p");
+    p.textContent = bolo.getDescricao();
+    pedidosDiv.appendChild(p);
+
+    form.reset();
+});
